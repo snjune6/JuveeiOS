7ac111abbed102f99ad0b373f0bbf9c589bee486
kkx2 <snjune6@gmail.com>
2023-06-12 09:11:06 +0900

    Initial Commit

---
+++ juvee/juveeApp.swift
@@ -0,0 +1,17 @@
+//
+//  juveeApp.swift
+//  juvee
+//
+//  Created by 이미연 on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct juveeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
